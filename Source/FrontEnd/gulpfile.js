4ea69d23e9db87f6171c3130347ff4cb3c19d878
Kevin Main <kevmain@outlook.com>
2016-02-12 10:16:06 +0000

    Added jshint to gulp pipeline

--- Source/FrontEnd/gulpfile.js
+++ Source/FrontEnd/gulpfile.js
@@ -3,11 +3,13 @@ var connect = require('gulp-connect');
 var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 
 gulp.task('scripts', function() {
     del.sync(['Assets/js/app.js']);
 
     return gulp.src(['App/app.module.js', 'App/**/**.config.js', 'App/**/**.module.js', 'App/**/**.factory.js', 'App/**/**.js'])
+    .pipe(jshint())
     .pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(gulp.dest('Assets'));
