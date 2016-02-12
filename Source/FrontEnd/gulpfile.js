86912b23712bd92cc4ff80ad185eb0543719e180
Kevin Main <kevmain@outlook.com>
2016-02-12 10:07:55 +0000

    Added Uglify to gulp pipeline

--- Source/FrontEnd/gulpfile.js
+++ Source/FrontEnd/gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 var del = require('del');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function() {
     del.sync(['Assets/js/app.js']);
 
     return gulp.src(['App/app.module.js', 'App/**/**.config.js', 'App/**/**.module.js', 'App/**/**.factory.js', 'App/**/**.js'])
     .pipe(concat('app.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('Assets'));
 });
 
