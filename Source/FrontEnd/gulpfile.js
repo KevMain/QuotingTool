357832c18aebcff595200d51cd57b566fecdca30
Kevin Main <kevmain@outlook.com>
2016-02-12 19:40:59 +0000

    Changed to use gulp generated JS!

--- Source/FrontEnd/gulpfile.js
+++ Source/FrontEnd/gulpfile.js
@@ -4,17 +4,23 @@ var del = require('del');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var gutil = require('gulp-util')
 
 gulp.task('scripts', function() {
+    gutil.log('Deleting scripts');
+    
     del.sync(['Assets/js/app.js']);
 
-    return gulp.src(['App/app.module.js', 'App/**/**.config.js', 'App/**/**.module.js', 'App/**/**.factory.js', 'App/**/**.js'])
+    gutil.log('Building scripts');
+    
+    return gulp.src(['App/app.module.js', 'App/**/**.module.js', 'App/**/**.config.js', 'App/**/**.factory.js', 'App/**/**.js'])
     .pipe(jshint())
     .pipe(concat('app.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('Assets'));
+    .pipe(gulp.dest('Assets/js'));
 });
 
 gulp.task('default', ['scripts'], function () {
+    gutil.log('Starting up server');
     connect.server();
 });
