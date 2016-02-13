9ed1ec4d4590cb09331b8977dbcefc61e809cfa6
Kevin Main <kevmain@outlook.com>
2016-02-13 09:43:31 +0000

    Added Styles and gulp task

--- Source/FrontEnd/gulpfile.js
+++ Source/FrontEnd/gulpfile.js
@@ -8,16 +8,41 @@ var gutil = require('gulp-util')
 
 gulp.task('scripts', function() {
     gutil.log('Deleting scripts');
-    
     del.sync(['Assets/js/app.js']);
 
     gutil.log('Building scripts');
-    
     return gulp.src(['App/app.module.js', 'App/**/**.module.js', 'App/**/**.config.js', 'App/**/**.factory.js', 'App/**/**.js'])
     .pipe(jshint())
     .pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(gulp.dest('Assets/js'));
+});
+
+gulp.task('styles', function() {
+    gutil.log('Deleting styles');
+    del.sync(['Assets/css/main.css']);
+
+    gutil.log('Building styles');
+    return gulp
+        .src('App/Styles/*.less')
+        //.pipe(plugins.plumber())
+        //.pipe(plugins.less())
+        //.pipe(plugins.autoprefixer(config.autoprefixer))
+        //.pipe(config.debug ? util.noop() : plugins.shorthand())
+        //.//pipe(plugins.rename({
+         //   suffix: noCache
+        //}))
+        //.pipe(gulp.dest(config.paths.styles.pub))
+        //.pipe(plugins.rename({
+        //    suffix: '.min'
+        //}))
+        //.pipe(plugins.sourcemaps.init())
+        //.pipe(plugins.bytediff.start())
+        //.pipe(config.debug ? util.noop() : plugins.csso())
+        //.pipe(plugins.bytediff.stop(bytediffFormatter))
+        //.pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest('Assets/css'))
+        //.pipe(sync.reload({stream:true}));
 });
 
 gulp.task('default', ['scripts'], function () {
