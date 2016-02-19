a52feaf0c442b245fb8229b572fb5fd7c47d1a75
Kevin Main <kevmain@outlook.com>
2016-02-19 15:33:00 +0000

    Improved Gulp CSS task

--- Source/FrontEnd/gulpfile.js
+++ Source/FrontEnd/gulpfile.js
@@ -5,12 +5,13 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var gutil = require('gulp-util')
+var less = require('gulp-less');
+var shorthand = require('gulp-shorthand');
+var rename = require("gulp-rename");
 
 gulp.task('scripts', function() {
-    gutil.log('Deleting scripts');
     del.sync(['Assets/js/app.js']);
 
-    gutil.log('Building scripts');
     return gulp.src(['App/app.module.js', 'App/**/**.module.js', 'App/**/**.config.js', 'App/**/**.factory.js', 'App/**/**.js'])
     .pipe(jshint())
     .pipe(concat('app.js'))
@@ -19,33 +20,17 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('styles', function() {
-    gutil.log('Deleting styles');
     del.sync(['Assets/css/main.css']);
 
-    gutil.log('Building styles');
     return gulp
         .src('App/Styles/*.less')
-        //.pipe(plugins.plumber())
-        //.pipe(plugins.less())
-        //.pipe(plugins.autoprefixer(config.autoprefixer))
-        //.pipe(config.debug ? util.noop() : plugins.shorthand())
-        //.//pipe(plugins.rename({
-         //   suffix: noCache
-        //}))
-        //.pipe(gulp.dest(config.paths.styles.pub))
-        //.pipe(plugins.rename({
-        //    suffix: '.min'
-        //}))
-        //.pipe(plugins.sourcemaps.init())
-        //.pipe(plugins.bytediff.start())
-        //.pipe(config.debug ? util.noop() : plugins.csso())
-        //.pipe(plugins.bytediff.stop(bytediffFormatter))
-        //.pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest('Assets/css'))
-        //.pipe(sync.reload({stream:true}));
+        .pipe(less())
+        .pipe(shorthand())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('Assets/css'));
 });
 
-gulp.task('default', ['scripts'], function () {
+gulp.task('default', ['scripts', 'styles'], function () {
     gutil.log('Starting up server');
     connect.server();
 });
