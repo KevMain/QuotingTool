c3760b789c2bdaaeed4a1f57b75a3f14835b32ca
Kevin Main <kevmain@outlook.com>
2016-07-22 16:12:34 +0100

    Added console hosting for API

---
+++ Source/API/CompatibleSoftware.QuotingTool.Host/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CompatibleSoftware.QuotingTool.API;
+using Microsoft.Owin.Hosting;
+
+namespace CompatibleSoftware.QuotingTool.Host
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (WebApp.Start<Bootstrap>("http://localhost:9876"))
+            {
+                Console.WriteLine(@"Web Server is running.");
+                Console.WriteLine(@"Press any key to quit.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
